265614024b958b19aa3f98bcad92cca885847775
Koh Jiefeng <koh_jiefeng@live.com>
2015-09-16 11:02:25 +0800

    added dailyreportextractor

---
+++ DailyReportExtractor/DailyReportExtractor/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace DailyReportExtractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LogTextbox = new System.Windows.Forms.RichTextBox();
+            this.errorStockTextBox = new System.Windows.Forms.RichTextBox();
+            this.stockTextbox = new System.Windows.Forms.RichTextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(153, 14);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(30, 31);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(735, 126);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 180);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Status";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(157, 180);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Error Stock List";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 180);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Stock List";
+            // 
+            // LogTextbox
+            // 
+            this.LogTextbox.Location = new System.Drawing.Point(297, 203);
+            this.LogTextbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.LogTextbox.Name = "LogTextbox";
+            this.LogTextbox.ReadOnly = true;
+            this.LogTextbox.Size = new System.Drawing.Size(592, 122);
+            this.LogTextbox.TabIndex = 10;
+            this.LogTextbox.Text = "";
+            // 
+            // errorStockTextBox
+            // 
+            this.errorStockTextBox.Location = new System.Drawing.Point(157, 203);
+            this.errorStockTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.errorStockTextBox.Name = "errorStockTextBox";
+            this.errorStockTextBox.ReadOnly = true;
+            this.errorStockTextBox.Size = new System.Drawing.Size(110, 122);
+            this.errorStockTextBox.TabIndex = 8;
+            this.errorStockTextBox.Text = "";
+            // 
+            // stockTextbox
+            // 
+            this.stockTextbox.Location = new System.Drawing.Point(23, 203);
+            this.stockTextbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.stockTextbox.Name = "stockTextbox";
+            this.stockTextbox.Size = new System.Drawing.Size(110, 122);
+            this.stockTextbox.TabIndex = 9;
+            this.stockTextbox.Text = "";
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.LightGreen;
+            this.StartButton.Enabled = false;
+            this.StartButton.Location = new System.Drawing.Point(13, 105);
+            this.StartButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(129, 35);
+            this.StartButton.TabIndex = 14;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd MMM yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(13, 32);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(129, 26);
+            this.dateTimePicker1.TabIndex = 15;
+            this.dateTimePicker1.Value = new System.DateTime(2015, 1, 1, 0, 0, 0, 0);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Starts From:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ALL",
+            "FullYearResults",
+            "HalfYearResults",
+            "1stQuarterResults",
+            "2ndQuarterResults",
+            "3rdQuarterResults",
+            "4thQuarterResults",
+            "AGM"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(126, 28);
+            this.comboBox1.TabIndex = 17;
+            this.comboBox1.Text = "ALL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 339);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LogTextbox);
+            this.Controls.Add(this.errorStockTextBox);
+            this.Controls.Add(this.stockTextbox);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "AnnualReportExtractor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox LogTextbox;
+        private System.Windows.Forms.RichTextBox errorStockTextBox;
+        private System.Windows.Forms.RichTextBox stockTextbox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
